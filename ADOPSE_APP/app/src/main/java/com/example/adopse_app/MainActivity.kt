4fc503311747f2bef6e1cb28fa5156584c5e5671
Dimitris Giouvanakis <dimitris121997@gmail.com>
2024-03-14 19:18:01 +0200

    secondActivity

--- ADOPSE_APP/app/src/main/java/com/example/adopse_app/MainActivity.kt
+++ ADOPSE_APP/app/src/main/java/com/example/adopse_app/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.adopse_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +11,17 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+            var btnSecondActivity = findViewById<Button>(R.id.btnSecondActivity)
+
+            btnSecondActivity.setOnClickListener {
+                val intent = Intent(this, SecondActivity::class.java)
+                startActivity(intent)
+            }
+
+
         }
     }
-}
---
+++ ADOPSE_APP/app/src/main/java/com/example/adopse_app/SecondActivity.kt
@@ -0,0 +1,16 @@
+package com.example.adopse_app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+    class SecondActivity : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+
+            setContentView(R.layout.activity_second)
+
+
+
+        }
+    }
