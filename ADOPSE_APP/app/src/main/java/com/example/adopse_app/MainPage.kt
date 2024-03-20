c97549f25e9b32fadecd6e395c823735143e5ffd
Dimitris Giouvanakis <dimitris121997@gmail.com>
2024-03-20 20:44:17 +0200

    build

--- ADOPSE_APP/app/src/main/java/com/example/adopse_app/MainPage.kt
+++ ADOPSE_APP/app/src/main/java/com/example/adopse_app/MainPage.kt
@@ -11,6 +11,7 @@ class MainPage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.main_page)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
