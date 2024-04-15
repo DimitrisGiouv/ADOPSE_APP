0b1f84b93c736a2c582e1a65f5bbc092827a1d39
Dean Joanidhi <deanj.development@gmail.com>
2024-04-15 16:49:01 +0300

    addition of a parameter to watch the phone bezels

--- ADOPSE_APP/app/src/main/java/com/example/adopse_app/UserProfileActivity.kt
+++ ADOPSE_APP/app/src/main/java/com/example/adopse_app/UserProfileActivity.kt
@@ -1,4 +1,39 @@
 package com.example.adopse_app
 
-class UserProfileActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class UserProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_profile_student)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.profile_student)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        /* Redirection to Sign up if user does not have an account */
+        val tempRedirect = findViewById<ImageButton>(R.id.user_profile_picture)
+
+        tempRedirect.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        /* Navigation Hamburger */
+        val navigationButton = findViewById<ImageButton>(R.id.navigation_button)
+        navigationButton.setOnClickListener {
+            val intent = Intent(this, NavigationActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
