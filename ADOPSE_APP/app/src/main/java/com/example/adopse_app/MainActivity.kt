ae5f64f95eb7529412404b649aa19ae765d40ab1
Sotos2013 <www.sotihatzi@gmail.com>
2024-04-23 00:14:39 +0300

    Διορθωση της αναζητησης

--- ADOPSE_APP/app/src/main/java/com/example/adopse_app/MainActivity.kt
+++ ADOPSE_APP/app/src/main/java/com/example/adopse_app/MainActivity.kt
@@ -31,13 +31,19 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        //Search bar να γινει μια παραπανω υλοποιηση
-        val searchBar = findViewById<EditText>(R.id.SearchBar)
-
-        val logo = findViewById<Button>(R.id.searchButton)
-        logo.setOnClickListener{
-            performSearchByCategory(searchBar.text.toString())
+        // Εισαγωγή ακροατή γεγονότος στο EditText του search bar
+        val searchEditText = findViewById<EditText>(R.id.SearchBar)
+        searchEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                // Καλέστε τη λειτουργία αναζήτησης με το νέο κείμενο που εισήχθη
+                val searchTerm = searchEditText.text.toString()
+                performSearchByCategory(searchTerm)
+                true
+            } else {
+                false
+            }
         }
+
 
 
 
@@ -49,7 +55,7 @@ class MainActivity : AppCompatActivity() {
         }
         // Κουμπί που μεταφέρει στην οθόνη πλοήγησης
         val navigationButton = findViewById<ImageButton>(R.id.navigation_button)
-            navigationButton.setOnClickListener {
+           navigationButton.setOnClickListener {
            val intent = Intent(this, NavigationActivity::class.java)
            startActivity(intent)
         }
@@ -238,7 +244,13 @@ class MainActivity : AppCompatActivity() {
         val parentLayout: ConstraintLayout = findViewById(R.id.LinearModules)
         parentLayout.removeAllViews()
 
-        val url = "http://10.0.2.2:5051/module/stack/18000/0/"  // Ολόκληρο URL για το αίτημα
+        if (searchTerm.isBlank()) {
+            Toast.makeText(this, "Η αναζήτηση είναι κενή", Toast.LENGTH_SHORT).show()
+            twoModuleList()
+            return
+        }
+
+        val url = "http://10.0.2.2:5051/module/stack/1000/0/"  // Ολόκληρο URL για το αίτημα
         val queue = Volley.newRequestQueue(this)
 
         val request = JsonObjectRequest(Request.Method.GET, url, null,
@@ -246,34 +258,49 @@ class MainActivity : AppCompatActivity() {
                 var foundModules = false
                 val modulesArray = response.getJSONArray("modules") // Ανάκτηση του πίνακα modules από την απόκριση JSON
 
-                var prevModuleId = ConstraintLayout.LayoutParams.PARENT_ID
+                // Αρχικοποίηση του rowIndex και columnIndex
+                var rowIndex = 0
+                var columnIndex = 0
 
                 for (i in 0 until modulesArray.length()) {
                     val moduleObject = modulesArray.getJSONObject(i) // Ανάκτηση του κάθε module από τον πίνακα modules
                     val subCategory = moduleObject.getInt("subCategoryId")
                     var subCategoryName = ""
 
-                    // Αντιστοίχιση του subCategoryId με το όνομα της κατηγορίας
+                    // Έλεγχος αν ο όρος αναζήτησης ταιριάζει με το όνομα της υποκατηγορίας
                     when (subCategory) {
-                        1 -> subCategoryName = "ΜηχανικήμηχανικηΜΗΧΑΝΙΚΗ"
-                        2 -> subCategoryName = "Διοίκηση επιχειρήσεων - Οικονομικά"
-                        3 -> subCategoryName = "Πληροφορική"
-                        4 -> subCategoryName = "Περιβάλλον και αειφορία"
-                        5 -> subCategoryName = "Παραιατρικά"
-                        6 -> subCategoryName = "Τουρισμός και φιλοξενία"
-                        7 -> subCategoryName = "Βιβλιοθηκονομία και συστήματα πληροφόρησης"
-                        8 -> subCategoryName = "Εφοδιαστική αλυσίδα και διαχείριση παραγωγής"
-                        9 -> subCategoryName = "Σχεδίαση προϊόντων και αισθητική"
-                        10 -> subCategoryName = "Παιδαγωγικά"
-                        11 -> subCategoryName = "Νομική - θεωρητικά"
-                        12 -> subCategoryName = "θετικές επιστήμες"
-                        else -> subCategoryName = "Unknown"
+                        // Κατηγορία: Μηχανική
+                        in listOf(3, 12, 19, 20, 21, 22, 37, 39, 44, 45, 51, 56, 60, 62, 66, 67, 86, 104, 112, 115, 121, 122, 125, 131, 132, 134, 136, 140, 144, 147) -> subCategoryName = "Μηχανική μηχανικη ΜΗΧΑΝΙΚΗ"
+                        // Κατηγορία: Πληροφορική
+                        in listOf(1, 6, 38, 92, 100, 103, 111, 114, 117, 118, 119, 124, 143, 153) -> subCategoryName = "Πληροφορική πληροφορικη ΠΛΗΡΟΦΟΡΙΚΗ"
+                        // Κατηγορία: Διοίκηση επιχειρήσεων - Οικονομικά
+                        in listOf(2, 5, 7, 8, 9, 13, 23, 24, 27, 29, 31, 41, 42, 49, 53, 58, 59, 69, 91, 98, 105, 120, 128, 130, 135, 137, 142, 150) -> subCategoryName = "Διοίκηση επιχειρήσεων - Οικονομικά ΔΙΟΙΚΗΣΗ ΕΠΙΧΙΡΗΣΕΩΝ ΟΙΚΟΝΟΜΙΚΑ διοικηση επιχειρησεων οικονομικα"
+                        // Κατηγορία: Περιβάλλον και αειφορία
+                        in listOf(4, 11, 16, 54, 61, 63, 65, 81, 83, 84, 85, 90, 93, 106, 107, 108, 109, 110, 126, 127, 149, 151, 154) -> subCategoryName = "Περιβάλλον και αειφορία περιβαλλον και αειφορια ΠΕΡΙΒΑΛΛΟΝ ΚΑΙ ΑΕΙΦΟΡΙΑ"
+                        // Κατηγορία: Παραιατρικά
+                        in listOf(10, 14, 32, 33, 36, 46, 48, 64, 68, 70, 71, 72, 75, 76, 77, 80, 97, 141) -> subCategoryName = "Παραιατρικά ΠΑΡΑΙΑΤΡΙΚΑ παραιατρικα"
+                        // Κατηγορία: Τουρισμός και φιλοξενία
+                        in listOf(26, 50, 55, 89, 139) -> subCategoryName = "Τουρισμός και φιλοξενία ΤΟΥΡΙΣΜΟΣ ΚΑΙ ΦΙΛΟΞΕΝΙΑ τουρισμος και φιλοξενια"
+                        // Κατηγορία: Βιβλιοθηκονομία και συστήματα πληροφόρησης
+                        in listOf(15, 18, 73, 74, 94, 145) -> subCategoryName = "Βιβλιοθηκονομία και συστήματα πληροφόρησης βιβλιοθηκονομια και συστηματα πληροφορησης ΒΙΒΛΙΟΘΗΚΟΝΟΜΙΑ ΚΑΙ ΣΥΣΤΗΜΑΤΑ ΠΛΗΡΟΦΟΡΗΣΗΣ"
+                        // Κατηγορία: Εφοδιαστική αλυσίδα και διαχείριση παραγωγής
+                        in listOf(17, 25, 35, 43, 57, 88, 95, 148, 155) -> subCategoryName = "Εφοδιαστική αλυσίδα και διαχείριση παραγωγής ΕΦΟΔΙΑΣΤΙΚΗ ΑΛΥΣΙΔΑ ΚΑΙ ΔΙΑΧΕΙΡΙΣΗ ΠΑΡΑΓΩΓΗΣ εφοδιαστικη αλυσιδα και διαχειριση παραγωγης"
+                        // Κατηγορία: Σχεδίαση προϊόντων και αισθητική
+                        in listOf(28, 30, 40, 99, 101) -> subCategoryName = "Σχεδίαση προϊόντων και αισθητική σχεδιαση προιοντων και αισθητικη ΣΧΕΔΙΑΣΗ ΠΡΟΪΟΝΤΩΝ ΚΑΙ ΑΙΣΘΗΤΙΚΗ"
+                        // Κατηγορία: Παιδαγωγικά
+                        in listOf(34, 47, 79, 102, 113, 129, 138) -> subCategoryName = "Παιδαγωγικά παιδαγωγικα ΠΑΙΔΑΓΩΓΙΚΑ"
+                        // Κατηγορία: Νομική - θεωρητικά
+                        in listOf(52, 78, 82, 87, 116, 123) -> subCategoryName = "Νομική - θεωρητικά νομικη - θεωρητικα"
+                        // Κατηγορία: θετικές επιστήμες
+                        in listOf(96, 133, 146) -> subCategoryName = "θετικές επιστήμες θετικες επιστημες ΘΕΤΙΚΕΣ ΕΠΙΣΤΗΜΕΣ"
+                        else -> subCategoryName = "" // Εκχώρηση κενού string στην περίπτωση που η υποκατηγορία δεν ανήκει σε καμία από τις παραπάνω κατηγορίες
                     }
 
+
+
                     // Έλεγχος αν ο όρος αναζήτησης ταιριάζει με το όνομα της υποκατηγορίας
-                    if (subCategoryName.contains(searchTerm, ignoreCase = true)) {
+                    if (!subCategoryName.isNullOrEmpty() && subCategoryName.contains(searchTerm, ignoreCase = true)) {
                         val moduleName = moduleObject.getString("name")
-                        //val description = moduleObject.getString("description")
                         val created = moduleObject.getString("created")
 
                         val moduleCard = layoutInflater.inflate(R.layout.module_long, null) as ConstraintLayout
@@ -296,17 +323,26 @@ class MainActivity : AppCompatActivity() {
 
                         layoutParams.setMargins(10, 30, 10, 10)
 
-                        if (prevModuleId != ConstraintLayout.LayoutParams.PARENT_ID) {
-                            layoutParams.topToBottom = prevModuleId
-                            layoutParams.startToStart = ConstraintLayout.LayoutParams.PARENT_ID
-                        }
+                        // Ορισμός του moduleCard στο grid layout
+                        layoutParams.startToStart = ConstraintLayout.LayoutParams.PARENT_ID
+                        layoutParams.topToBottom = if (columnIndex == 0 && rowIndex == 0) R.id.RecommendBlock else parentLayout.getChildAt(parentLayout.childCount - 1).id
 
+                        // Προσθήκη του moduleCard στο parentLayout
                         parentLayout.addView(moduleCard, layoutParams)
-                        prevModuleId = moduleCard.id
+
+                        // Αύξηση του columnIndex
+                        columnIndex++
+
+                        // Έλεγχος για να δούμε αν πρέπει να αυξήσουμε το rowIndex και να επαναφέρουμε το columnIndex στο 0
+                        if (columnIndex == 2) {
+                            columnIndex = 0
+                            rowIndex++
+                        }
 
                         foundModules = true
                     }
                 }
+
                 if (!foundModules) {
                     Toast.makeText(this, "Δεν υπάρχουν μαθήματα στην κατηγορία που αναζητήσατε!", Toast.LENGTH_SHORT).show()
                     twoModuleList()
@@ -319,5 +355,6 @@ class MainActivity : AppCompatActivity() {
 
         queue.add(request)
     }
+
 
 }
