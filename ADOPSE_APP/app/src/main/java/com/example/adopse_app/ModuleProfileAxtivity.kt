2f0cf62ea7746e23ffb04e5e1f06582fb9a31b64
Dimitris Giouvanakis <dimitris121997@gmail.com>
2024-04-28 16:57:00 +0300

    moduleprofilepage note finish

---
+++ ADOPSE_APP/app/src/main/java/com/example/adopse_app/ModuleProfileAxtivity.kt
@@ -0,0 +1,24 @@
+package com.example.adopse_app
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ModuleProfileAxtivity : AppCompatActivity() {
+    private var page = 0
+    private val pageSize = 10 // Μέγεθος σελίδας
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_moduleprofile)
+        // Καθορισμός των περιοχών των system bars
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.moduleProfile)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
