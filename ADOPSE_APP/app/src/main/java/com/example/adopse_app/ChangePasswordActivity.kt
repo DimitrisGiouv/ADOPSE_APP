e656dd3d1be7a0917e83bfa984903a3162107848
j <giorgosmelas2002@gmail.com>
2024-05-14 18:30:25 +0300

    password change fixed

--- ADOPSE_APP/app/src/main/java/com/example/adopse_app/ChangePasswordActivity.kt
+++ ADOPSE_APP/app/src/main/java/com/example/adopse_app/ChangePasswordActivity.kt
@@ -23,87 +23,74 @@ class ChangePasswordActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_change_password)
 
-        val username = findViewById<EditText>(R.id.username)
-        val email = findViewById<EditText>(R.id.email)
-        val newPwd = findViewById<EditText>(R.id.newPassword)
+        val usernameField = findViewById<EditText>(R.id.username)
+        val emailField = findViewById<EditText>(R.id.email)
+        val newPwdField = findViewById<EditText>(R.id.newPassword)
         val submitBtn = findViewById<TextView>(R.id.submitBtn)
 
         submitBtn.setOnClickListener{
-            val usernameText = username.text.toString().trim()
-            val emailText = email.text.toString().trim()
-            val newPwdText = newPwd.text.toString().trim()
+            val usernameText = usernameField.text.toString().trim()
+            val emailText = emailField.text.toString().trim()
+            val newPwdText = newPwdField.text.toString().trim()
 
             if(usernameText.isEmpty() || newPwdText.isEmpty() || emailText.isEmpty()){
                 Toast.makeText(this, "Please fill the fields", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
 
-            // Make a request to get all users
-            val usersUrl = "http://10.0.2.2:5051/Authentication/getUsers"
-            val usersRequest = JsonArrayRequest(Request.Method.GET, usersUrl, null,
+            if(newPwdText.length < 8){
+                Toast.makeText(this, "Password has to be more than 8 characters.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if(!hasSpecialCharAndUpper(newPwdText)){
+                Toast.makeText(this, "Password has to contain special characters and uppers", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val sharedPreferences = getSharedPreferences("myAppPref", Context.MODE_PRIVATE)
+            val username = sharedPreferences.getString("username", "")
+            val email = sharedPreferences.getString("email", "")
+            val id = sharedPreferences.getInt("id", -1)
+
+            if(usernameText != username || emailText != email){
+                Toast.makeText(this, "User not found", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val Items = JSONObject()
+            Items.put("id", id)
+            Items.put("username", usernameText)
+            Items.put("email", emailText)
+            Items.put("password", newPwdText)
+
+
+            val queue = Volley.newRequestQueue(this)
+            val updateUrl = "http://10.0.2.2:5051/Authentication/updateUser"
+            val request = JsonObjectRequest(Request.Method.POST, updateUrl, Items,
                 { response ->
-                    // Handle successful response
-                    var userFound = false
-                    var userJsonObject: JSONObject? = null
-
-                    // Iterate through users to find the user with the same username
-                    for (i in 0 until response.length()) {
-                        val user = response.getJSONObject(i)
-                        val userUsername = user.getString("username")
-
-                        if (userUsername == usernameText) {
-                            // User found
-                            userFound = true
-                            userJsonObject = user
-                            break
-                        }
-                    }
-
-                    if (userFound) {
-                        // User found, proceed with changing password
-                        // Construct JSON object with new password and user details
-                        val userPasswordUpdate = JSONObject()
-
-                        // Add username, email, and id to userPasswordUpdate
-                        userPasswordUpdate.put("username", userJsonObject?.getString("username"))
-                        userPasswordUpdate.put("email", userJsonObject?.getString("email"))
-                        userPasswordUpdate.put("id", userJsonObject?.getString("id"))
-
-                        // Update password
-                        userPasswordUpdate.put("password", newPwdText)
-
-                        // Make request to update user's password
-                        val queue = Volley.newRequestQueue(this)
-                        val updateUrl = "http://10.0.2.2:5051/Authentication/updateUser"
-                        val request = JsonObjectRequest(Request.Method.POST, updateUrl, userPasswordUpdate,
-                            { response ->
-                                // Password changed successfully
-                                Toast.makeText(this,"Password changed successfully", Toast.LENGTH_SHORT).show()
-                                Handler().postDelayed({
-                                    val intent = Intent(this, MainActivity::class.java)
-                                    startActivity(intent)
-                                },2000)
-                            },
-                            { error ->
-                                // Error updating password
-                                Toast.makeText(this,"Failed to update password", Toast.LENGTH_SHORT).show()
-                            })
-                        queue.add(request)
-                    } else {
-                        // User not found
-                        Toast.makeText(this, "User not found", Toast.LENGTH_SHORT).show()
-                    }
+                    // Password changed successfully
+                    Toast.makeText(this,"Password changed successfully", Toast.LENGTH_SHORT).show()
+                    Handler().postDelayed({
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    },2000)
                 },
                 { error ->
-                    // Handle error
-                    Toast.makeText(this,"Failed to retrieve users", Toast.LENGTH_SHORT).show()
+                    // Error updating password
+                    Toast.makeText(this,"Failed to update password", Toast.LENGTH_SHORT).show()
                 })
-
-            // Add the request to the RequestQueue
-            Volley.newRequestQueue(this).add(usersRequest)
+            queue.add(request)
         }
+    }
 
+    fun hasSpecialCharAndUpper(password: String): Boolean{
+        val specialCharRegex = Regex("[!@#$%^&*(){}:;><.,?]")
+        val upperRegex = Regex("[A-Z]")
 
+        val containsSpecialChar = specialCharRegex.containsMatchIn(password)
+        val containsUpper = upperRegex.containsMatchIn(password)
 
+        return containsSpecialChar && containsUpper
     }
 }
--- ADOPSE_APP/app/src/main/java/com/example/adopse_app/SignupActivity.kt
+++ ADOPSE_APP/app/src/main/java/com/example/adopse_app/SignupActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.Toast
@@ -11,6 +12,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import org.json.JSONObject
@@ -62,6 +64,53 @@ class SignupActivity :AppCompatActivity() {
             val request = JsonObjectRequest (Request.Method.POST,url,Items,
                 { response ->
                     Toast.makeText(this, "Welcome ${usernameText}", Toast.LENGTH_SHORT).show()
+                    // Find user's details
+                    val usersUrl = "http://10.0.2.2:5051/Authentication/getUsers"
+                    val usersRequest = JsonArrayRequest(Request.Method.GET, usersUrl, null,
+                        { response ->
+                            var userFound = false
+                            var userJsonObject: JSONObject? = null
+
+                            // Iterate through users to find the user with the same username
+                            for (i in 0 until response.length()) {
+                                val user = response.getJSONObject(i)
+                                val userUsername = user.getString("username")
+
+                                if (userUsername == usernameText) {
+                                    // User found
+                                    userFound = true
+                                    userJsonObject = user
+                                    break
+                                }
+                            }
+
+                            if (userFound) {
+                                val sharedPreferences = getSharedPreferences("myAppPref", MODE_PRIVATE)
+                                with(sharedPreferences.edit()){
+                                    putString("username", userJsonObject?.getString("username"))
+                                    putString("email", userJsonObject?.getString("email"))
+                                    putInt("id", userJsonObject?.getInt("id")?: -1)
+                                    apply()
+                                }
+
+                                val username1 = sharedPreferences.getString("username", "")
+                                val email = sharedPreferences.getString("email", "")
+                                val id = sharedPreferences.getInt("id", -1)
+
+                                Log.d("SharedPreferences", "Username: $username1")
+                                Log.d("SharedPreferences", "Email: $email")
+                                Log.d("SharedPreferences", "ID: $id")
+                            } else {
+                                // User not found
+                                Toast.makeText(this, "User not found", Toast.LENGTH_SHORT).show()
+                            }
+                        },
+                        { error ->
+                            // Handle error
+                            Toast.makeText(this,"Failed find user's details", Toast.LENGTH_SHORT).show()
+                        })
+
+                    Volley.newRequestQueue(this).add(usersRequest)
                     android.os.Handler().postDelayed({
                         val intent = Intent(this, MainActivity::class.java)
                         startActivity(intent)
